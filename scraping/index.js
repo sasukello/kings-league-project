c22ec835ab7253f3d56532e3cd6047d2a93d1067
Anderson Montilla <montillaanderson@gmail.com>
2023-01-17 20:46:13 -0300

    Add scraper for leaderboard

---
+++ scraping/index.js
@@ -0,0 +1,59 @@
+//esta libreria se utiliza para trabajar la pagina web obtenida del scraping
+import * as cheerio from 'cheerio'
+import { writeFile } from 'node:fs/promises'
+import path from 'node:path'
+
+
+const URLS = {
+    leaderboard : 'https://kingsleague.pro/estadisticas/clasificacion/'
+}
+
+async function scrape (url){
+    const res = await fetch(url)
+    const html = await res.text()
+    return cheerio.load(html)
+
+}
+async function getLeaderBoard(){
+    const $ = await scrape(URLS.leaderboard)
+    //se obtiene el valor de una tabla de la pagina en una variable de cheerio
+    const $rows = $('table tbody tr')
+
+    //esta constante se puede modificar , si llega a cambiar los selectores de la tabla 
+    const LEADERBOARD_SELECTOR ={
+        team: {selector:'.fs-table-text_3', typeOf:'string'},
+        wins: {selector:'.fs-table-text_4', typeOf:'number'},
+        loses: {selector:'.fs-table-text_5', typeOf:'number'},
+        scoredGoals: {selector:'.fs-table-text_6', typeOf:'number'},
+        concededGoals: {selector:'.fs-table-text_7', typeOf:'number'},
+        yellowCards: {selector:'.fs-table-text_8', typeOf:'number'},
+        redCards: {selector:'.fs-table-text_9', typeOf:'number'}
+    }
+
+    const cleanText = text => text
+        .replace(/\t|\n|\s:/g, '')
+        .replace(/.*:/g, ' ')
+        .trim()
+
+
+    const leaderBoardSelectorEntries = Object.entries(LEADERBOARD_SELECTOR)
+
+    let leaderboard =[]
+    $rows.each((index, el)=>{
+        const $el = $(el)
+        const leaderBoardEntries = leaderBoardSelectorEntries.map(([key, { selector, typeOf }]) => {
+        const rawValue = $el.find(selector).text()
+        const cleanedValue = cleanText(rawValue)
+
+        const value = typeOf === 'number' ? Number(cleanedValue): cleanedValue
+        return [key, value]
+      })
+      leaderboard.push(Object.fromEntries(leaderBoardEntries))
+    })
+    return leaderboard  
+    
+}
+  const leaderboard =  await getLeaderBoard()
+  const filePath = path.join(process.cwd(), './db/leaderboard.json')
+  
+  await writeFile(filePath,JSON.stringify(leaderboard, null , 2), 'utf-8')
